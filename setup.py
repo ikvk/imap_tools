780566875243b64d199817bb262c4f611263b1d4
kvk <v.kaukin@u6.ru>
2017-05-31 09:07:13 +0500

    long_description

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='imap_tools',
-    version='0.1.1',
+    version='0.1.4',
     packages=['imap_tools'],
     url='https://github.com/ikvk/imap_tools',
     license='MIT',
+    long_description=open("README.rst").read(),
     author='v.kaukin',
     author_email='workkvk@gmail.com',
     description='Tool for work with e-mail messages and not with the imap protocol.',
