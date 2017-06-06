d96080e240c825b9298d85161f686329dcf3949a
kvk <v.kaukin@u6.ru>
2017-06-06 13:07:45 +0500

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='imap_tools',
-    version='0.1.6',
+    version='0.1.7',
     packages=['imap_tools'],
     url='https://github.com/ikvk/imap_tools',
     license='MIT',
