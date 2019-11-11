4b492605a5d1ecbeda824f6534bf60e1c38252f6
ikvk <KaukinVK@ya.ru>
2019-11-11 21:58:26 +0500

    fix license in setup

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setuptools.setup(
     keywords=['imap', 'imap-client', 'python3', 'email'],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)",
         "Operating System :: OS Independent",
     ],
     # install_requires=['typing>=3.6.2'],
