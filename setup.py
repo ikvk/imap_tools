a6ae1005f56b4c5eceeaedddda6a13a23be6c713
ikvk <KaukinVK@ya.ru>
2019-11-11 22:01:25 +0500

    fix license in setup

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.rst", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='imap_tools',
-    version='0.9.6',
+    version='0.9.7',
     packages=setuptools.find_packages(),
     url='https://github.com/ikvk/imap_tools',
-    license='Apache-2.0',
+    license='Apache Software License 2.0 (Apache-2.0)',
     long_description=long_description,
     long_description_content_type="text/x-rst",
     author='v.kaukin',
@@ -17,6 +17,7 @@ setuptools.setup(
     keywords=['imap', 'imap-client', 'python3', 'email'],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License 2.0 (Apache-2.0)",
         "Operating System :: OS Independent",
     ],
     # install_requires=['typing>=3.6.2'],
