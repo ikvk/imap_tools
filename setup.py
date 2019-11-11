da422b72bf459446cbb6626fe57cdd25c787e475
ikvk <KaukinVK@ya.ru>
2019-11-11 21:43:09 +0500

    fix license in setup

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ with open("README.rst", "r", encoding='utf-8') as fh:
 
 setuptools.setup(
     name='imap_tools',
-    version='0.9.4',
+    version='0.9.5',
     packages=setuptools.find_packages(),
     url='https://github.com/ikvk/imap_tools',
-    license='MIT',
+    license='Apache-2.0',
     long_description=long_description,
     long_description_content_type="text/x-rst",
     author='v.kaukin',
