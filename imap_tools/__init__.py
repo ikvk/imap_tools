3d4aefdff72c8730721724ce8e5c0cc6ddf08c56
Vladimir Kaukin <KaukinVK@ya.ru>
2020-07-13 20:07:25 +0500

    add deprecated Q to default import, *forgot =)

--- imap_tools/__init__.py
+++ imap_tools/__init__.py
@@ -1,8 +1,8 @@
-from .query import AND, OR, NOT, Header, A, O, N, H
+from .query import Q, AND, OR, NOT, Header, A, O, N, H
 from .mailbox import *
 from .message import *
 from .folder import *
 from .utils import *
 from .errors import *
 
-__version__ = '0.18.0'
+__version__ = '0.18.1'
