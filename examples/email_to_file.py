e624bf8163eb0566ec63fc937596e0d231af5f8e
v.kaukin <v.kaukin@u6.ru>
2020-09-30 09:07:02 +0500

    improve examples

---
+++ examples/email_to_file.py
@@ -0,0 +1,10 @@
+"""
+
+If you want to save message as eml, read email lib docs
+
+work with Message.obj
+
+    https://docs.python.org/3/library/email.message.html#email.message.EmailMessage.as_string
+    https://docs.python.org/3/library/email.message.html#email.message.EmailMessage.as_bytes
+
+"""
