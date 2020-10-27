ed33a12c9fa29377dc24e02881ad2c71ba29f7bf
v.kaukin <v.kaukin@u6.ru>
2020-10-27 16:00:39 +0500

    minor

--- examples/fetch_by_pages.py
+++ examples/fetch_by_pages.py
@@ -6,8 +6,8 @@ with MailBox('imap.mail.com').login('test@mail.com', 'pwd', 'INBOX') as mailbox:
     page_len = 3
     pages = int(len(found_nums) // page_len) + 1 if len(found_nums) % page_len else int(len(found_nums) // page_len)
     for page in range(pages):
-        print('= page {} ='.format(page))
+        print('page {}'.format(page))
         page_limit = slice(page * page_len, page * page_len + page_len)
         print(page_limit)
         for msg in mailbox.fetch(criteria, bulk=True, limit=page_limit):
-            print(msg.subject, msg.date_str)
+            print(' ', msg.date, msg.uid, msg.subject)
