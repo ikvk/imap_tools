f8378bbabe13cf03c50ce63025d2867de71a13e9
v.kaukin <v.kaukin@u6.ru>
2021-02-02 12:06:14 +0500

    new example parse_eml_attachments

--- examples/parse_eml_attachments.py
+++ examples/parse_eml_attachments.py
@@ -9,3 +9,22 @@ with MailBox('imap.mail.com').login('test@mail.com', 'password') as mailbox:
                 eml_email = MailMessage.from_bytes(att.payload)
                 print('- subject: ', eml_email.subject)
                 print('- date_str: ', eml_email.date_str)
+
+# parse .eml files from attached .eml files (2 levels of nesting)
+with open('/tmp/1.eml', 'rb') as f:
+    bytes_data = f.read()
+msg = MailMessage.from_bytes(bytes_data)
+for att in msg.attachments:
+    if '.eml' in att.filename:
+        # 1 level .eml
+        msg_level_1 = MailMessage.from_bytes(att.payload)
+        print(att.filename)
+        print('* subject: ', msg_level_1.subject)
+        print('* date: ', msg_level_1.date)
+        for att_sub in msg_level_1.attachments:
+            if '.eml' in att_sub.filename:
+                # 2 level .eml
+                msg_level_2 = MailMessage.from_bytes(att_sub.payload)
+                print('|--', att_sub.filename)
+                print('|   * subject: ', msg_level_2.subject)
+                print('|   * date: ', msg_level_2.date)
